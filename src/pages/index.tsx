51a9af00333f321e1bd1a94161b067fbe1d15202
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-28 13:05:01 +0200

    add first buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
 import { io, Socket } from 'socket.io-client'
 
 export default function Home() {
-  console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
-  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000';
-  const socket: Socket = io(backendUrl, { autoConnect: false });
-  // socket.disconnect();
-
-  // fetch('http://localhost:3000/').then(response => console.log('ok', response.ok));
+  // const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000';
+  // const socket: Socket = io(backendUrl, { autoConnect: false });
 
   return (
     <>
@@ -18,8 +13,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-
+      <main className='w-full h-full bg-blue flex flex-col justify-center items-center gap-4'>
+        <h1 className='text-xl'>Slap game</h1>
+        <button className='btn'>Créer partie</button>
+        <button className='btn'>Rejoindre partie</button>
       </main>
     </>
   )
