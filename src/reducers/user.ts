feeec9cb2722c9d97e7ea39d4517dd8bbebaf5af
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-29 10:54:27 +0200

    create user reducer

---
+++ src/reducers/user.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface UserState {
+  username: string;
+  email: string;
+};
+
+const initialState: UserState = {
+  username: '',
+  email: '',
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addUsername: (state, action: PayloadAction<string>) => {
+      state.username = action.payload;
+    },
+    removeUser: (state) => {
+      state = initialState;
+    }
+  },
+});
+
+export const { } = userSlice.actions;
+
+export default userSlice.reducer;
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import user from '@/reducers/user';
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    user,
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
