70a135b0b4f362f0ee1c0409dc167d8c1e5f39d3
Camille HAUSTANT <camillehaustant@gmail.com>
2023-06-06 23:11:11 +0200

    add redirect on error

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -9,7 +9,9 @@ socket.onAny((event, ...args) => {
 
 socket.on("connect_error", (err) => {
   console.log('Socket error: ', err.message); // not authorized
+  alert('Error occured, please try again');
   socket.disconnect();
+  window.location.href = '/';
 });
 
 export default socket;
