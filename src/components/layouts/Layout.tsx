59451c37e28471ebbe9e72f43485868d719920d5
Camille HAUSTANT <camillehaustant@gmail.com>
2023-06-06 23:10:30 +0200

    prefix title

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -2,15 +2,16 @@ import React, { PropsWithChildren } from 'react';
 import Head from 'next/head';
 
 type Props = PropsWithChildren & {
-  title: string;
+  title?: string;
   description: string;
 };
 
-export default function Layout({ children, title, description }: Props) {
+export default function Layout({ children, title = "", description }: Props) {
+  const titleDisplayed = title ? `Slap Game - ${title}` : 'Slap Game';
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{titleDisplayed}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
